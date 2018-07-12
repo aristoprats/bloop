22c204a0c131b41668e5929f1e8e289d0e425ee0
Harry To <to000002@umn.edu>
2018-07-12 14:34:31 +0700

    z works now

--- functional_scripts/computer_recieve_from_ext.py
+++ functional_scripts/computer_recieve_from_ext.py
@@ -5,7 +5,7 @@ Purpose: recieves inputs from external terminal (same machine), prints inputs
 
 Definitions:
 host - where this script is being run
-guest - terminal where the connection is made from
+guest - terminal where the conn is made from
 
 Exit Mode: press 'z' on script side
 terminal prompt: telnet localhost 5555
@@ -18,8 +18,8 @@ import sys
 from _thread import *
 
 
-def send_recieve(connection):
-    connection.send(str.encode("Enter input \n"))
+def send_recieve(conn):
+    conn.send(str.encode("Enter input \n"))
 
     while True:
         data = conn.recv(4096)  #something about size of data buffer?
@@ -38,23 +38,22 @@ def send_recieve(connection):
             #prints statement on guest side
         conn.sendall(str.encode('\n'))
 
-        if (input.lower() == 'z'):
-            #Terminates connection if user inputs 'z' and exits main function loop
+        if (input.lower()[0] == 'z'):
+            #Terminates conn if user inputs 'z' and exits main function loop
             print("Connection ended")
             conn.close()
             active = False
-            break
+            return
 
 
 def main():
-    #####Global Variables
     host = ''  #left blank to refer to localhost for now
     port = 5555     #random port
-    max_connections = 5
+    max_conns = 5
     active = True
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #initializes connection point as (tcp,synchronous)
+        #initializes conn point as (tcp,synchronous)
 
     print("Waiting for Connection")
 
@@ -63,10 +62,10 @@ def main():
     except socket.error as error:
         print(str(error))         #prints out error for troubleshooting
 
-    s.listen(max_connections)  #limits max number of queued max_connections
+    s.listen(max_conns)  #limits max number of queued max_conns
     while (active == True):
         conn, addr = s.accept()
-            #accepts a connection as it comes in, returns (connection, address)
+            #accepts a conn as it comes in, returns (conn, address)
         print("Connection to ",addr[0]," initialized at ", addr[1])
 
         start_new_thread(send_recieve,(conn,))
