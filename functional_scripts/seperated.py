aa6236b0d2102e6585194faca18963e553f1f995
Prats Mathur <prats.mathur@gmail.com>
2018-07-12 23:52:01 -0700

    Changed function names and added comments for readability

--- functional_scripts/seperated.py
+++ functional_scripts/seperated.py
@@ -9,7 +9,7 @@ guest - terminal where the conn is made from
 
 Exit Mode: press 'z' on script side
 
-terminal prompt: telnet localhost 5555
+terminal prompt: telnet localhost 5551
 
 Requires 2 terminals open
 """
@@ -19,17 +19,17 @@ import sys
 from _thread import *
 
 '''(1)
-def receive(conn):
+def receive_host(conn):
     #Recieves an input from the guest side and prints said input to host
     #console
 
     conn.send(str.encode("Enter input \n"))
 
     while True:
-        recieved = conn.recv(4096)
+        recieved = conn.recv(4096) #sets buffer size to 4096 bytes
         input = str(recieved.decode('utf-8'))
         input = input[0 : len(input) - 2]
-        input = '---------- ' + input
+        input = '---------- ' + input #distinguishes input from output on host side
         conn.sendall(str.encode('\n'))
 
         if not recieved:
@@ -37,27 +37,32 @@ def receive(conn):
         print(input)
 
         if (input.lower()[-1] == 'z'):
+            # FIXME: terminates connection when last letter is 'z'
             print("Guest terminated Connection")
             conn.close()
             break
 '''
 '''(2)
-def send(conn):
+def send_host(conn):
     #exports an input from host side and prints it on guest side
     print("Waiting for input\n")
     output_last = ''
     while True:
         output = str(input(""))
         output = '---------- ' + output
+            #helps distinguish input from output on guest terminal
         if (output == output_last):
+            #speeds up program and reduces memory use by only acting upon change
             pass
         else:
 
             conn.sendall(str.encode(output))
             conn.sendall(str.encode('\n'))
-        output_last = output
+
+        output_last = output        #used to limt send to only on change of state
 
         if (output.lower()[-1] == 'z'):
+            # FIXME: Breaks connection when last letter is 'z'
             print("Host terminated Connection")
             conn.close()
             break
@@ -69,12 +74,14 @@ def main():
     max_conns = 5
 
     s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #establishes connection as TCP and asynchronous
 
     print("Waiting for Guest to connect")
 
     try:
         s.bind((host,port))
     except socket.error as sError:
+        #returns socket binding error in text form to help debug if needed
         print(str(sError))
         return
 
@@ -82,8 +89,10 @@ def main():
     while True:
         conn, addr = s.accept()
         print("Connection to ",addr[0]," initialized at ",addr[1])
-        #(1) start_new_thread(receive,(conn,))
-        #(2) start_new_thread(send,(conn,))
+        #(1) start_new_thread(receive_host,(conn,))
+            #starts thread for host to recieve from guest
+        #(2) start_new_thread(send_host,(conn,))
+            #starts thread for guest to receive from host
 
 if __name__ == '__main__':
     main()
