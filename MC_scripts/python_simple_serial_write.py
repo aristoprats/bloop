9ebad6b9f1856cb03bbda26a6952b54db5f11875
aristoprats <prats.mathur@gmail.com>
2018-07-23 16:36:30 -0700

    Works with a static message, option for user input commented out atm

--- MC_scripts/python_simple_serial_write.py
+++ MC_scripts/python_simple_serial_write.py
@@ -1,16 +1,30 @@
 import serial
 import time
 
-PORT = '/dev/tty.usbmodem1411'
+PORT = '/dev/ttyACM0'
 baud_rate = 9600
 
 ser = serial.Serial(PORT, baud_rate)
+print("Connection established to port: " + PORT)
 
 time.sleep(3)
+counter = 0;
 
+#msg = input("Enter input: \n")
 
-while True:
-    ser.write(b'1')
-    time.sleep(1)
-    ser.write(b'0')
-    time.sleep(1)
+msg = "Hello, How are you today?!"
+msg_chopped = []
+
+def msg_chopper(message):
+    for letter in message:
+        msg_chopped.append(letter)
+
+while (counter < 25):
+    msg_chopped.clear()
+    msg_chopper(msg)
+    for value in msg_chopped:
+        ser.write(bytes(value,'utf-8'))
+        time.sleep(.5)
+    counter += 1
+
+ser.close()
