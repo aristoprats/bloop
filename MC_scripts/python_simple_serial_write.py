95a05425731cfc3e07371ecc093bfbb90c3b0e21
aristoprats <prats.mathur@gmail.com>
2018-07-23 18:28:39 -0700

    Added comments and made things more testing friendly

--- MC_scripts/python_simple_serial_write.py
+++ MC_scripts/python_simple_serial_write.py
@@ -1,30 +1,34 @@
 import serial
 import time
 
-PORT = '/dev/ttyACM0'
-baud_rate = 9600
+PORT = '/dev/ttyACM0' # REVIEW: figure out how to automate finding this dev path
+baud_rate = 9600      #data transmission rate, no idea why its called baud for MCUs
+    #IMPORTANT: This has to match the read and write rate on the MC chip
 
 ser = serial.Serial(PORT, baud_rate)
 print("Connection established to port: " + PORT)
 
 time.sleep(3)
-counter = 0;
+#needed because opening the serial port resets the MCU so it needs time to reinitialize
 
 #msg = input("Enter input: \n")
 
-msg = "Hello, How are you today?!"
+msg = "Hello, World!"
 msg_chopped = []
 
 def msg_chopper(message):
+    #REVIEW: doesn't need to be a function rn but may make things cleaner later
     for letter in message:
         msg_chopped.append(letter)
 
-while (counter < 25):
-    msg_chopped.clear()
-    msg_chopper(msg)
-    for value in msg_chopped:
-        ser.write(bytes(value,'utf-8'))
-        time.sleep(.5)
-    counter += 1
+msg_chopped.clear()
+msg_chopper(msg)
+
+for value in msg_chopped:
+    ser.write(bytes(value,'utf-8'))
+    print(value)
+    time.sleep(.5)
+
 
 ser.close()
+quit()
